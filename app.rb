585f8a8b267b0f4897590c6e59e8611c3082278a
riky moroni <riky5.moroni@gmail.com>
2021-12-02 15:44:24 +0000

    step 20: refactor

--- app.rb
+++ app.rb
@@ -6,15 +6,23 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @Player_1 = params[:Player_1]
-    @Player_2 = params[:Player_2]
-    erb :play
+    session[:Player_1] = params[:Player_1]
+    session[:Player_2] = params[:Player_2]
+    redirect '/play'
   end
   
+  get '/play' do
+    @Player_1 = session[:Player_1]
+    @Player_2 = session[:Player_2]
+    erb :play
+  end
+
   run! if app_file == $0
 end
