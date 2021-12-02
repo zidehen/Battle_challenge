b32c7bf6b529bc447935e5324ce01b71ea9d56b6
zidehen <zoeidehen@gmail.com>
2021-12-02 15:57:54 +0000

    step 21- player 2 hp added

---
+++ spec/features/view_hit_points_spec.rb
@@ -0,0 +1,9 @@
+feature "Player 2 Hit points" do
+  scenario 'be able to see player 2 hit points' do
+    visit('/')
+    fill_in('Player_1', with: 'David')
+    fill_in('Player_2', with: 'Tom')
+    click_button 'Submit'
+    expect(page).to have_content('Tom: 20 HP')
+    end
+end
