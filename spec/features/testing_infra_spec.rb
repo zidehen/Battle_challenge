f3637f2fdf62fb03c4ee0302c8c9d22fe346c200
zidehen <zoeidehen@gmail.com>
2021-12-02 13:31:43 +0000

    updated feature test set up -step 18

--- spec/features/testing_infra_spec.rb
+++ spec/features/testing_infra_spec.rb
@@ -1,9 +1,8 @@
-feature Battle do
-  context 'homepage' do
-    it 'displays "Testing infrastructure working!"' do
+
+feature 'Testing infrastructure' do
+    scenario 'can visit the homepage and check it has content' do
       visit('/')
       expect(page).to have_content("Testing infrastructure working!")
     end
-  end
 
 end
